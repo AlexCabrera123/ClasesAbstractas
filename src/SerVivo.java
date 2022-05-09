4508c0500960b57e8cd4d25b19ccbd2c2e7ac4f4
AlexCabrera123 <l21121600@morelia.tecnm.mx>
2022-05-08 20:17:02 -0500

    Initial commit

---
+++ src/SerVivo.java
@@ -0,0 +1,13 @@
+public abstract class SerVivo {
+//atributos, getters, setters, constructor
+
+    public void comer(){
+        System.out.println("Comiendo");
+    }
+
+    //hablar lo realizan de forma diferente
+    //hablar debería ser abstracto
+
+    public abstract void hablar();
+
+}
